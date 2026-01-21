4c689abd0e340aa69434b2aa0ec8e6d3e95e2bfe
JK-VISWAS <joyviswaskolli@gmail.com>
2026-01-21 21:34:48 +0530

    changed main

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes } from 'react-router-dom';
 import { Route } from 'lucide-react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
