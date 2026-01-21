dc1932d25439a7c780c99ebf394f1b8d26562c84
JK-VISWAS <joyviswaskolli@gmail.com>
2026-01-21 23:52:21 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import 'leaflet/dist/leaflet.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
